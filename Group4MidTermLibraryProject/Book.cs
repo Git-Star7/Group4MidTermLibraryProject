17849377a25fa0ee6306f6b465bb94ef6ec654c0
Michael <Michael@acdmail.com>
2020-02-06 14:29:46 -0500

    Added Book Class and Inherited Media Class

---
+++ Group4MidTermLibraryProject/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group4MidTermLibraryProject
+{
+    class Book : Media
+    {
+        //fields+properties
+        public string Author { get; set; }
+
+        //constructors
+        public Book(string author)
+        {
+            Author = author;
+        }
+
+        //methods
+        
+    }
+}
---
+++ Group4MidTermLibraryProject/Media.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group4MidTermLibraryProject
+{
+    class Media
+    {
+
+    }
+}
