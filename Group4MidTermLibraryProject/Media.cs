0507fb3788ab07c2f4bd95948a78b625188f89de
heather <hjohnsick@yahoo.com>
2020-02-07 09:02:26 -0500

    2ndcommit

--- Group4MidTermLibraryProject/Media.cs
+++ Group4MidTermLibraryProject/Media.cs
@@ -5,13 +5,43 @@ using System.Text;
 namespace Group4MidTermLibraryProject
 {
     abstract class Media
-    {
+    { 
         //properties
-        public string MediaType { get; set; }
+       
         public string Title { get; set; }
-        public DateTime DueDate {get; set;}
+
+        public string Author { get; set; }
+        public DateTime DueDate { get; set; }
         public string Status { get; set; }
 
+       
+        public static void PrintList(List<Media> print)
+        {
+
+            for (int i = 0; i < print.Count; i++)
+            {
+               
+                Console.WriteLine();
+                Console.WriteLine($"\t{print[i].Title}");
+                Console.WriteLine($"\t{print[i].Author}");
+                Console.WriteLine($"\t{print[i].DueDate}");
+                Console.WriteLine($"{print[i].Status}");
+                Console.WriteLine();
+
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
 
     }
+
+   
+  
+
+  
 }
